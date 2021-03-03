91a4ea261cdbd96cd3ee8b45334972e4cf98f416
ryois <ryois@ryois.me>
2021-03-03 15:36:57 -0500

    Auth, certs, students, homepage

---
+++ src/views/certs.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    </div>
+</template>
+<script>
+export default {
+    name: "certs",
+    data: function () {
+        return {
+        };
+    },
+    methods: {
+    },
+    mounted: function () {
+    },
+};
+</script>
+<style scoped>
+</style>
