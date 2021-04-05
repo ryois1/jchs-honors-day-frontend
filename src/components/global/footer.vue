daca728f757eec5e5430a2fb68d4c2da0b00aadf
Ryois <ryois@ryois.me>
2021-04-05 13:22:40 +0000

    Update images and text

--- src/components/global/footer.vue
+++ src/components/global/footer.vue
@@ -2,10 +2,10 @@
     <footer class="footer mt-auto py-3">
         <b-navbar type="dark" variant="dark">
             <b-navbar-nav>
-                <b-nav-text>Designed and developed by Russell Payne 2021</b-nav-text>
+                <b-nav-text class="text-white">Designed and developed by Russell Payne 2021</b-nav-text>
             </b-navbar-nav>
             <b-navbar-nav class="ml-auto">
-                <b-nav-text>For help, please contact Alan Reece at areece@jcboe.net or at Main Campus extension 1611</b-nav-text>
+                <b-nav-text class="text-white">For help, please contact Alan Reece at areece@jcboe.net or at Main Campus extension 1611</b-nav-text>
             </b-navbar-nav>
         </b-navbar>
     </footer>
