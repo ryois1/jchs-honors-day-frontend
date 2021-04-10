3f60aeff6f2d94fd645e19583572f0a260c0b841
ryois <ryois@ryois.me>
2021-04-10 16:01:30 -0400

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-const SentryCliPlugin = require('@sentry/webpack-plugin');
-
 module.exports = {
     chainWebpack: config => {
         config
@@ -9,17 +7,5 @@ module.exports = {
                 return args
             })
     },
-    plugins: [
-        new SentryCliPlugin({
-          include: '.',
-          ignore: ['node_modules', 'webpack.config.js'],
-          url: 'https://sentry.ryois.net/',
-          authToken: 'f7ee8a9a999a11eb8edd0242ac120014',
-          token: 'f7ee8a9a999a11eb8edd0242ac120014',
-          org: 'jcs',
-          project: 'honors-day-frontend',
-          debug: true
-        }),
-    ],
     productionSourceMap: true,
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
-const SentryCliPlugin = require('@sentry/webpack-plugin');
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 
 module.exports = {
-  plugins: [
-    new SentryCliPlugin({
-      include: '.',
-      ignore: ['node_modules', 'webpack.config.js'],
-      url: 'https://sentry.ryois.net/',
-      authToken: 'f7ee8a9a999a11eb8edd0242ac120014',
-      token: 'f7ee8a9a999a11eb8edd0242ac120014',
-      org: 'jcs',
-      project: 'honors-day-frontend',
-      debug: true
-    }),
-  ],
-};
+    configureWebpack: {
+      plugins: [
+        new SentryWebpackPlugin({
+            token: 'f7ee8a9a999a11eb8edd0242ac120014',
+            authToken: 'f7ee8a9a999a11eb8edd0242ac120014',
+            org: "jcs",
+            url: "https://sentry.ryois.net/",
+            debug: true,
+            project: "honors-day-frontend",
+            include: "./dist",
+            ignore: ["node_modules", "webpack.config.js"],
+        }),
+      ],
+    },
+  };
