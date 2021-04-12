0c0e312b691b220ddcd7321c217d09c7b8f8155a
Ryois <ryois@ryois.me>
2021-04-12 13:24:17 +0000

    Add clickable email

--- src/components/global/footer.vue
+++ src/components/global/footer.vue
@@ -5,7 +5,7 @@
                 <b-nav-text class="text-white">Designed and developed by Russell Payne 2021</b-nav-text>
             </b-navbar-nav>
             <b-navbar-nav class="ml-auto">
-                <b-nav-text class="text-white">For help, please contact Alan Reece at areece@jcboe.net or at Main Campus extension 1611</b-nav-text>
+                <b-nav-text class="text-white">For help, please contact Alan Reece at <u><a class="" target="_blank" href="https://outlook.office.com/mail/0/deeplink/compose?to=areece@jcboe.net&subject=Awards%20Portal%20Help">areece@jcboe.net</a></u> or at Main Campus extension 1611</b-nav-text>
             </b-navbar-nav>
         </b-navbar>
     </footer>
