18672cb72161a9688b458d7c3a733acf0392a7a6
Ryois <ryois@ryois.me>
2021-04-15 12:32:30 +0000

    Add mobile notice

---
+++ src/components/mobile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+            <b-navbar-brand><b-img-lazy src="https://firebasestorage.googleapis.com/v0/b/jchs-honors-day.appspot.com/o/web%2Fjchs_patriots_logo_web.png?alt=media" width="30" height="30" alt="JCHS Patriots Mascot" class="d-inline-block align-top"></b-img-lazy>JCHS Awards Portal</b-navbar-brand>
+        </b-navbar>
+        <b-container>
+            <b-row>
+                <b-col>
+                    <h1 class="text-center mobile">The Awards Portal does not work on a phone, please use a desktop or laptop.</h1>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+<style scoped>
+.navbar.navbar-dark.bg-dark {
+  background-color: #2E3191 !important;
+}
+</style>
