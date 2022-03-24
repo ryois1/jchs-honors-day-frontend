5b25d71f730ca726265e86c07453091d3e96ce8f
Ryois <ryois@ryoisme>
2022-03-24 14:25:24 -0400

    I am big stupid

--- src/App.vue
+++ src/App.vue
@@ -107,7 +107,7 @@ export default {
       onCancel: false,
       isLoading: true,
       ADMINS: ["ADMIN", "COMMITTEE", "DEPT_ADMIN"],
-      VUE_APP_GIT_HASH: process.env.VUE_APP_COMMIT_HASH,
+      VUE_APP_VER: process.env.VUE_APP_VER,
     };
   },
   computed: {
